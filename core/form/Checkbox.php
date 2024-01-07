af1a9f26ae95c696777e100de758320f2eda5216
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-07 19:58:31 +0700

    feat(form): enhance Checkbox component, support value and label configuration fck.

--- core/form/Checkbox.php
+++ core/form/Checkbox.php
@@ -8,33 +8,56 @@ class Checkbox
 {
     public Model $model;
     public string $name;
+    public mixed $value;
     public bool $checked;
-    public string $label;
+    public string|null $label;
+    public bool $useLabel;
 
-    public function __construct(Model $model, $label = null, string $name, bool $checked = false)
+    public function __construct(Model $model, string $name, mixed $value, bool $checked = false, string|null $label = null, bool $useLabel = false)
     {
         $this->model = $model;
         $this->name = $name;
+        $this->value = $value;
         $this->checked = $checked;
         $this->label = $label;
+        $this->useLabel = $useLabel;
     }
 
     public function __toString()
     {
+        $checkedAttribute = $this->valueMatchesModel() ? 'checked' : '';
+
         return sprintf(
             '
             <div class="form-control">
                 <label class="cursor-pointer label">
-                    <span class="label-text">%s</span>
-                    <input type="checkbox" name="%s" value="%s" %s class="checkbox %s" />
+                    <div class="flex gap-2">
+                        <input type="checkbox" name="%s" value="%s" %s class="checkbox %s" />
+                        %s
+                    </div>
                 </label>
+                <div class="label">
+                    <span class="label-text-alt">%s</span>
+                </div>
             </div>
         ',
-            $this->label ?? text_alt_formatter($this->name),
             $this->name,
-            $this->model->{$this->name},
-            $this->checked ? 'checked' : '',
+            $this->value,
+            $checkedAttribute,
             $this->model->hasError($this->name) ? 'checkbox-error' : '',
+            $this->label($this->useLabel),
+            $this->model->getFirstError($this->name)
         );
+    }
+
+    private function valueMatchesModel(): bool
+    {
+        $modelValue = is_array($this->model->{$this->name}) ? in_array($this->value, $this->model->{$this->name}) : $this->model->{$this->name};
+        return $this->checked ? $modelValue : !$modelValue;
+    }
+
+    private function label(bool $useLabel)
+    {
+        return $useLabel ? \sprintf('<span class="label-text">%s</span>', $this->label ?? text_alt_formatter($this->name)) : '';
     }
 }
