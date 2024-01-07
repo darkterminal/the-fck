cf02ac9e61ca0a9df6314c4da3a2cfdc2d1027e7
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-07 19:59:21 +0700

    feat(form): enhance Input class, add support for dynamic label, placeholder, and classes fck.

--- core/form/Input.php
+++ core/form/Input.php
@@ -10,20 +10,25 @@ class Input
     public Model $model;
     public string $type = 'text';
     public string $name;
-    public string $classes;
+    public bool $useLabel;
+    public string|null $label;
+    public string|null $placeholder;
+    public string|array $classes;
 
     public array $types = ['text', 'password', 'email', 'date', 'time'];
 
-    public function __construct(Model $model, string $type, string $name, string $classes)
+    public function __construct(Model $model, string $type, string $name, string|null $label = null, bool $useLabel = true, string|null $placeholder = null, string|array $classes = [])
     {
         if (in_array($type, $this->types)) {
             $this->model = $model;
-            $this->type = $type;
             $this->name = $name;
+            $this->useLabel = $useLabel;
+            $this->type = $type;
+            $this->label = $label;
+            $this->placeholder = $placeholder;
             $this->classes = $classes;
         } else {
             throw new Exception("Error undefined the types of input. The allowed input is ". implode(', ', $this->types) ."", 1);
-            
         }
     }
 
@@ -32,23 +37,33 @@ class Input
         return sprintf(
             '
             <label class="form-control w-full">
-                <div class="label">
-                    <span class="label-text">%s</span>
-                </div>
-                <input type="%s" name="%s" placeholder="Type %s here" value="%s" class="input input-bordered w-full %s %s" />
+                %s
+                <input type="%s" name="%s" placeholder="Type %s here" value="%s" class="%s %s" />
                 <div className="label">
                     <span className="label-text-alt">%s</span>
                 </div>
             </label>
         ',
-            text_alt_formatter($this->name),
+            $this->label($this->useLabel),
             $this->type,
             $this->name,
-            text_alt_formatter($this->name),
+            text_alt_formatter($this->placeholder ?? $this->name),
             $this->model->{$this->name},
-            $this->classes,
+            twMerge('input input-bordered w-full', $this->classes),
             $this->model->hasError($this->name) ? 'input-error' : '',
             $this->model->getFirstError($this->name)
         );
+    }
+
+    private function label(bool $useLabel)
+    {
+        $label = sprintf('
+            <div class="label">
+                <span class="label-text">%s</span>
+            </div>
+        ',
+            $this->label ?? text_alt_formatter($this->name)
+        );
+        return $useLabel ? $label : '';
     }
 }
