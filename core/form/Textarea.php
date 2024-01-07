7947dacc08f8673553d81fb044415605b1e6377f
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-07 20:01:34 +0700

    feat(form): add Textarea class for handling textarea input fields fck.

--- core/form/Textarea.php
+++ core/form/Textarea.php
@@ -8,12 +8,18 @@ class Textarea
 {
     public Model $model;
     public string $name;
-    public string $classes;
+    public string|null $label;
+    public bool $useLabel;
+    public string|null $placeholder;
+    public string|array $classes;
 
-    public function __construct(Model $model, string $name, string $classes)
+    public function __construct(Model $model, string $name, string|null $label = null, bool $useLabel = true, string|null $placeholder = null, string|array $classes = [])
     {
         $this->model = $model;
         $this->name = $name;
+        $this->label = $label;
+        $this->useLabel = $useLabel;
+        $this->placeholder = $placeholder;
         $this->classes = $classes;
     }
 
@@ -21,22 +27,32 @@ class Textarea
     {
         return sprintf('
             <label class="form-control w-full">
-                <div class="label">
-                    <span class="label-text">%s</span>
-                </div>
-                <textarea name="%s" placeholder="Type %s here" class="textarea textarea-bordered w-full %s %s">%s</textarea>
+                %s
+                <textarea name="%s" placeholder="Type %s here" class="%s %s">%s</textarea>
                 <div className="label">
                     <span className="label-text-alt">%s</span>
                 </div>
             </label>
         ',
-            text_alt_formatter($this->name),
+            $this->label($this->useLabel),
             $this->name,
-            text_alt_formatter($this->name),
-            $this->classes,
+            text_alt_formatter($this->placeholder ?? $this->name),
+            \twMerge('textarea textarea-bordered w-full', $this->classes),
             $this->model->hasError($this->name) ? 'input-error' : '',
             $this->model->{$this->name},
             $this->model->getFirstError($this->name)
         );
+    }
+
+    private function label(bool $useLabel)
+    {
+        $label = sprintf('
+            <div class="label">
+                <span class="label-text">%s</span>
+            </div>
+        ',
+            $this->label ?? text_alt_formatter($this->name)
+        );
+        return $useLabel ? $label : '';
     }
 }
